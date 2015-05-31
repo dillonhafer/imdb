0d678384f2e3b2c9fbcdf49eac2e320353907f08
Dillon Hafer <dh@dillonhafer.com>
2015-05-31 17:41:42 -0400

    Add working commands

--- main.go
+++ main.go
@@ -6,7 +6,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
 )
+
+type Movie struct {
+	Id       string
+	Title    string
+	Director string
+	Year     string
+	Plot     string
+	Genre    string
+}
+
+type Tagger struct {
+	Movie    Movie
+	FilePath string
+	Format   string
+}
 
 func (m *Movie) GetImdbInfo() {
 	res, err := http.Get(m.ApiUrl())
@@ -28,23 +46,45 @@ func (m *Movie) ApiUrl() string {
 	return fmt.Sprintf(url, m.Id)
 }
 
-func (m *Movie) ParsleyFlags() string {
-	return fmt.Sprintf("--title \"%s\" --artist \"%s\" --year \"%s\" --description \"%s\" --genre \"%s\"", m.Title, m.Director, m.Year, m.Plot, m.Genre)
-}
-
-type Movie struct {
-	Id       string
-	Title    string
-	Director string
-	Year     string
-	Plot     string
-	Genre    string
+func (m *Movie) ParsleyFlags() []string {
+	return []string{"--title", m.Title, "--artist", m.Director, "--year", m.Year, "--description", m.Plot, "--genre", m.Genre}
 }
 
 func FindMovie(id string) Movie {
 	m := Movie{Id: id}
 	m.GetImdbInfo()
 	return m
+}
+
+func (t *Tagger) TempId() string {
+	var file_id string
+	files, _ := ioutil.ReadDir("./")
+	for _, f := range files {
+		r, _ := regexp.Compile("-temp-([0-9]+)")
+		if r.MatchString(f.Name()) {
+			file_id = r.FindString(f.Name())
+		}
+	}
+	return file_id
+}
+
+func (t *Tagger) AtomicCommand() error {
+	file_path := fmt.Sprintf("%s.%s", t.FilePath, t.Format)
+	file_args := []string{file_path}
+	args := append(file_args, t.Movie.ParsleyFlags()...)
+	_, err := exec.Command("AtomicParsley", args...).Output()
+	return err
+}
+
+func (t *Tagger) CleanupCommand() error {
+	old_file := fmt.Sprintf("%s%s.%s", t.FilePath, t.TempId(), t.Format)
+	new_file := fmt.Sprintf("%s.%s", t.FilePath, t.Format)
+	return os.Rename(old_file, new_file)
+}
+
+func (t *Tagger) SetTags() {
+	t.AtomicCommand()
+	t.CleanupCommand()
 }
 
 func main() {
@@ -58,8 +98,8 @@ func main() {
 
 	if id != "" && file_path != "" && format != "" {
 		m := FindMovie(id)
-		fmt.Printf("When this works it will run:\n")
-		fmt.Printf("AtomicParsley %s.%s %s", file_path, format, m.ParsleyFlags())
+		t := &Tagger{Movie: m, Format: format, FilePath: file_path}
+		t.SetTags()
 	} else {
 		flag.Usage()
 	}
