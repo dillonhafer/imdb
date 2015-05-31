4c69bae54d642ed58bb50a225638af55d04a9860
Dillon Hafer <dh@dillonhafer.com>
2015-05-31 15:05:44 -0400

    Make working JSON parser

--- main.go
+++ main.go
@@ -4,73 +4,42 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"os/exec"
 )
 
-type MinecraftRunner interface {
-	Run(cmd string) error
-}
-
-type DummyMinecraft struct {
-	Logger io.Writer
-}
-
-func (dm *DummyMinecraft) Run(cmd string) error {
-	_, err := fmt.Fprintf(dm.Logger, "DummyMinecraft: %v\n", cmd)
-	return err
-}
-
-type Minecraft struct {
-	Screen string
-}
-
-type MinecraftCmd struct {
-	Command string
-}
-
-type MinecraftCmdCollection struct {
-	Pool map[string]MinecraftCmd
-}
-
-func (mc *MinecraftCmdCollection) FromJson(jsonStr []byte) error {
-	var data = &mc.Pool
-	return json.Unmarshal(jsonStr, data)
-}
-
-func (m *Minecraft) Run(cmd string) error {
-	return exec.Command("screen", "-S", m.Screen, "-p", "0", "-X", "stuff", fmt.Sprintf(`/%s\r`, cmd)).Run()
-}
-
-func FindCmd(cmdName string) string {
-	mc := new(MinecraftCmdCollection)
-	content, err := ioutil.ReadFile("commands.json")
+func (m *Movie) GetImdbInfo() {
+	res, err := http.Get(m.ApiUrl())
 	if err != nil {
-		fmt.Print("Error:", err)
+		panic(err.Error())
 	}
-
-	mc.FromJson(content)
-	return mc.Pool[cmdName].Command
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
-func commandApi(mc MinecraftRunner) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprint(w, "Command Must be a POST\n")
-			return
-		}
+func (m *Movie) ApiUrl() string {
+	url := "http://www.omdbapi.com/?i=%s&plot=short&r=json"
+	return fmt.Sprintf(url, m.Id)
+}
 
-		cmd := FindCmd(r.URL.Path[4:])
-		err := mc.Run(cmd)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "Error: %v", err)
-			return
-		}
-	}
+func (m *Movie) Info() {
+	m.GetImdbInfo()
+	fmt.Printf("--title \"%s\"\n", m.Title)
+}
+
+type Movie struct {
+	Id          string
+	Title       string
+	Director    string
+	Year        string
+	Description string
+	Genre       string
 }
 
 func main() {
@@ -79,7 +48,8 @@ func main() {
 	flag.StringVar(&id, "id", "", "IMDB ID of movie. (e.g. tt1564349)")
 	flag.Parse()
 
-	if id == "" {
-		// Get info
+	if id != "" {
+		m := Movie{Id: id}
+		m.Info()
 	}
 }
