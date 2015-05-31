78b7dd0506ee6f556fd0d98fb9bd516cc46aad83
Dillon Hafer <dh@dillonhafer.com>
2015-05-31 19:24:15 -0400

    Use cli package

--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
-	"flag"
+	"github.com/codegangsta/cli"
+	"os"
+	"path"
 )
 
-func main() {
-	var id string
-	var file_path string
-	var format string
-	flag.StringVar(&id, "id", "", "IMDB ID of movie (e.g. tt1564349)")
-	flag.StringVar(&file_path, "file", "", "Path to video file")
-	flag.StringVar(&format, "format", "m4v", "File format of video file (defaults to m4v)")
-	flag.Parse()
+const VERSION = "0.0.1"
 
-	if id != "" && file_path != "" && format != "" {
-		m := FindMovie(id)
-		t := &Tagger{Movie: m, Format: format, FilePath: file_path}
-		t.SetTags()
-	} else {
-		flag.Usage()
+func main() {
+	app := cli.NewApp()
+	app.Name = "imdb"
+	app.Usage = "Import ID3 tags from IMDB"
+	app.Version = VERSION
+	app.Author = "Dillon Hafer"
+	app.Email = "dh@dillonhafer.com"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "id, i",
+			Usage: "imdb id of movie (e.g. tt1564349)",
+		},
 	}
+
+	app.Action = func(c *cli.Context) {
+		var file string
+		var format string
+		var id string
+		if len(c.Args()) == 1 {
+			given_file := c.Args()[0]
+			base := path.Base(given_file)
+			format = path.Ext(given_file)
+			file = base[:len(base)-len(format)]
+			id = c.String("id")
+		}
+
+		if id != "" && file != "" && format != "" {
+			println(id)
+			println(file)
+			println(format)
+			m := FindMovie(id)
+			t := &Tagger{Movie: m, Format: format, FilePath: file}
+			t.SetTags()
+		} else {
+			cli.ShowAppHelp(c)
+		}
+	}
+	app.Run(os.Args)
 }
